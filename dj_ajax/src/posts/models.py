0622055728848642fe614133c28479fb8804e73d
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2023-04-07 02:09:27 -0400

    Added capacity to hold photos in post
    Changes done:
    + The posts can contain photos now in the posts.
    + It has only the carousel for now.
    + Next step will be adding the drop zone.

--- dj_ajax/src/posts/models.py
+++ dj_ajax/src/posts/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from profiles.models import Profile
 
 # Create your models here.
+
+# This class will handle everything related to
+# the posts contents and data in our application
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
@@ -18,5 +21,20 @@ class Post(models.Model):
     def like_count(self):
         return self.liked.all().count()
     
+    def get_photos(self):
+        return self.photo_set.all()
+    
     class Meta:
         ordering = ("-created", )
+
+
+
+# This class will handle the addition of photos
+# to our posts
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="photos")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post.title}-{self.pk}"
