568cdbe2cfc9075d2e199039435cc8bffbab2614
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2023-04-14 00:21:07 -0400

    Images are added to our post now!
    Changes done:
    + Adding images to posts work now.
    + The photos appear after modal is closed and post details are opened.

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Photo
 from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
@@ -143,6 +143,7 @@ def image_upload_view(request):
         post = Post.objects.get(id=new_post_id)
         Photo.objects.create(image=img, post=post)
     return HttpResponse()
+
 
 
 # due to is_ajax being deprecated, I am doing my own
