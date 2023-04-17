d4659cabb197b908a90b6c2cd0077b8063b04f92
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2023-04-17 16:11:05 -0400

    Got rid of some unneeded prints.
    Changes done:
    The server was printing some unneeded stuff.

--- dj_ajax/src/posts/utils.py
+++ dj_ajax/src/posts/utils.py
@@ -3,16 +3,17 @@ from profiles.models import Profile
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+
 def action_permission(func):
     def wrapper(request, **kwargs):
         pk = kwargs.get('pk')
         profile = Profile.objects.get(user=request.user)
         post = Post.objects.get(pk=pk)
         if profile.user == post.author.user:
-            print('yes')
+            # print('yes')
             return func(request, **kwargs)
         else:
-            print('no')
+            # print('no')
             # return HttpResponse('access denied - you need to be the author of the post in order to delete it')
             return redirect('posts:main-board')
     
